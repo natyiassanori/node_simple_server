5163c75dc44adf060fd293f265922ee6c7935175
nnatsumy <=>
2022-03-07 00:35:23 -0300

    Changing logic to consider on the counter only the requests of last 60 seconds

--- index.js
+++ index.js
@@ -2,11 +2,37 @@ import express from 'express';
 import fs from 'fs';
 
 const server = express();
-let numberOfRequests;
+
+let requestDates = [];
+let nextExpirationDate = new Date();
+let nextComparsionInterval = 1;
+
+setInterval(() => { 
+    if(nextExpirationDate <= new Date() && requestDates.length !== 0){
+      requestDates.every(element => {
+        if(element <= nextExpirationDate){
+          requestDates.shift();
+          updateNextExpirationDateAndComparsionInterval();
+        }
+        else
+          return false;
+      });
+      writeOnFile();
+    }
+
+}, nextComparsionInterval);
+
 
 readFileAndUpdateNumberOfRequests();
 
-server.get('/', (req, res) =>{
+server.get('/requestCounter', (req, res) =>{
+
+  let expirationDate = new Date(new Date().getTime() + 1000 * 60);
+  if(requestDates.length === 0){
+    nextExpirationDate = expirationDate;
+  }
+  requestDates.push(expirationDate);
+
   getRequests().then(
     (val) => { 
       return res.json(val);
@@ -16,14 +42,13 @@ server.get('/', (req, res) =>{
 
 function getRequests() {
   return new Promise((resolve) => {
-    numberOfRequests++;
     writeOnFile();
-    resolve(numberOfRequests);
+    resolve(requestDates.length);
   })
 }
 
 function writeOnFile() {
-  fs.writeFile('./assets/requests.txt', numberOfRequests.toString(), (err) => {
+  fs.writeFile('./assets/requests.txt', requestDates.toString(), (err) => {
     if (err) throw new Error('Error writing to file.');
   });
 }
@@ -32,13 +57,32 @@ function readFileAndUpdateNumberOfRequests() {
   fs.readFile('./assets/requests.txt', 'utf8', (err, data) => {
     if(err){
       if(err.code == 'ENOENT' )
-        numberOfRequests = 0;      
+        requestDates = [];      
       else        
         throw new Error('Error reading file.');      
     }
-    else
-      numberOfRequests = data;    
+    else{
+      updateRequestDatesWithFileContent(data);  
+      updateNextExpirationDateAndComparsionInterval();
+    }
   });
+}
+
+function updateRequestDatesWithFileContent(data){
+  let dataArray = data.toString().split(",");
+      dataArray.forEach(element => {
+        if(isValidDate(new Date(element)))
+          requestDates.push(new Date(element));        
+      });      
+}
+
+function isValidDate(date) {
+  return !isNaN(date);
+}
+
+function updateNextExpirationDateAndComparsionInterval() {
+  nextExpirationDate = requestDates.length === 0 ? new Date() : requestDates[0];
+  nextComparsionInterval = requestDates.length === 0 ? 1000*60 : nextExpirationDate - new Date();
 }
 
 server.listen(3000);
